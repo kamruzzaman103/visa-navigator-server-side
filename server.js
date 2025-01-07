d80974561ccc1a7542b144ca5e8d64222554edc5
Kamruzzaman Tutul <kamruzzaman103>
2025-01-08 00:56:23 +0600

    Final update

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// server.js
-require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -15,10 +13,34 @@ app.use(cors()); // Enable CORS
 
 app.use(bodyParser.json());
 
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASS}@cluster0.2e5uu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Database Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(uri)
   .then(() => console.log('Database connected'))
   .catch(err => console.error('Database connection error:', err));
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Routes
 app.use('/api/visas', visaRoutes); // Visa-related routes
